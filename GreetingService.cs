e869db779995db01cf0fbbbb31d5605f3cd2f3ad
dennis <dennis.nilsson1111@gmail.com>
2026-02-19 21:57:26 +0100

    add unittest

---
+++ GreetingService.cs
@@ -0,0 +1,9 @@
+namespace MyWebApp;
+
+public static class GreetingService
+{
+    public static string GetGreeting(string name)
+    {
+        return $"Hello {name}!";
+    }
+}
---
+++ tests/MyWebApp.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWebApp.Tests;
+
+public class GreetingServiceTests
+{
+    [Fact]
+    public void GetGreeting_ReturnsExpectedMessage()
+    {
+        var result = GreetingService.GetGreeting("World");
+
+        Assert.Equal("Hello World!", result);
+    }
+}
